448dea08efc4fd9874726589186d6ab3956d94e8
mlpadilla <10867537+mlpadilla@users.noreply.github.com>
2021-05-14 19:46:17 +0200

    Remove Compose: No UI is going to be developed for now
    Compose + Kotlin 1.5.0 keep triggering issues. This is not the focus of this project, so there is no point in further maintenance.

--- app/src/main/java/com/mlpadilla/bowlinggamekata/MainActivity.kt
+++ app/src/main/java/com/mlpadilla/bowlinggamekata/MainActivity.kt
@@ -1,38 +1,5 @@
 package com.mlpadilla.bowlinggamekata
 
-import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.mlpadilla.bowlinggamekata.ui.theme.BowlingGameKataTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BowlingGameKataTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BowlingGameKataTheme {
-        Greeting("Android")
-    }
-}
+class MainActivity : ComponentActivity()
