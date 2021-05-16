d5f686a5b1744591ba8a4646a15febd3aa1e7e2e
mlpadilla <10867537+mlpadilla@users.noreply.github.com>
2021-05-16 15:12:45 +0200

    [DRAFT] Add rule: Tenth frame - allow to roll the extra balls

--- app/src/test/java/com/mlpadilla/bowlinggamekata/GameTest.kt
+++ app/src/test/java/com/mlpadilla/bowlinggamekata/GameTest.kt
@@ -1,13 +1,15 @@
 package com.mlpadilla.bowlinggamekata
 
+import com.mlpadilla.bowlinggamekata.game.Frame
 import com.mlpadilla.bowlinggamekata.game.Game
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 
 class GameTest : BehaviorSpec({
-    val game = Game()
 
     given("a number of pins knocked down") {
+        val game = Game()
         val pinsKnockedDown = 2
         `when`("calling roll") {
             game.roll(pinsKnockedDown)
@@ -15,6 +17,7 @@ class GameTest : BehaviorSpec({
     }
 
     given("no input") {
+        val game = Game()
         `when`("calling score") {
             val score = game.score()
             then("total score for that game is returned") {
@@ -24,8 +27,67 @@ class GameTest : BehaviorSpec({
     }
 
     given("a game") {
+        val game = Game()
         then("the game consists of 10 frames") {
             game.frames.size shouldBe 10
+        }
+    }
+
+    // In the tenth frame a player who rolls a spare or strike is allowed to roll the extra balls to complete the frame.
+    //TODO Update Game tests for roll/frame update sequence considering newly defined rules.
+    // Then enable these tests.
+    xgiven("a game on the tenth frame") {
+        `xwhen`("a player who rolls spare") {
+            val game = Game()
+            for(i in 0..8) {
+                game.roll(pinsKnockedDown = 10)
+            }
+            game.roll(pinsKnockedDown = 2)
+            game.roll(pinsKnockedDown = 8)
+            game.frames[9].let { tenthFrame ->
+                tenthFrame.shouldBeInstanceOf<Frame.FrameWithSpareBonus>()
+                tenthFrame.roll1!!.pinsKnockedDown shouldBe 2
+                tenthFrame.roll2!!.pinsKnockedDown shouldBe 8
+                tenthFrame.nextRoll shouldBe null
+            }
+            xthen("is allowed to roll the extra ball to complete the frame") {
+                game.roll(3)
+                game.frames[9].let { tenthFrame ->
+                    tenthFrame.shouldBeInstanceOf<Frame.FrameWithSpareBonus>()
+                    tenthFrame.roll1!!.pinsKnockedDown shouldBe 2
+                    tenthFrame.roll2!!.pinsKnockedDown shouldBe 8
+                    tenthFrame.nextRoll!!.pinsKnockedDown shouldBe 3
+                }
+            }
+        }
+        `xwhen`("a player who rolls strike") {
+            val game = Game()
+            for(i in 0..8) {
+                game.roll(pinsKnockedDown = 10)
+            }
+            game.roll(pinsKnockedDown = 10)
+            game.frames[9].let { tenthFrame ->
+                tenthFrame.shouldBeInstanceOf<Frame.FrameWithStrikeBonus>()
+                tenthFrame.roll1!!.pinsKnockedDown shouldBe 2
+                tenthFrame.nextRoll shouldBe null
+                tenthFrame.nextSecondRoll shouldBe null
+            }
+            then("is allowed to roll the extra balls to complete the frame") {
+                game.roll(3)
+                game.frames[9].let { tenthFrame ->
+                    tenthFrame.shouldBeInstanceOf<Frame.FrameWithStrikeBonus>()
+                    tenthFrame.roll1!!.pinsKnockedDown shouldBe 2
+                    tenthFrame.nextRoll shouldBe 3
+                    tenthFrame.nextSecondRoll shouldBe null
+                }
+                game.roll(4)
+                game.frames[9].let { tenthFrame ->
+                    tenthFrame.shouldBeInstanceOf<Frame.FrameWithStrikeBonus>()
+                    tenthFrame.roll1!!.pinsKnockedDown shouldBe 2
+                    tenthFrame.nextRoll shouldBe 3
+                    tenthFrame.nextSecondRoll shouldBe 4
+                }
+            }
         }
     }
 })
